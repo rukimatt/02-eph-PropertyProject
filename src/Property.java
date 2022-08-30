3fe53a7d091701c60d7af9081e6c2caecbfa0b48
rukimatt <rukimatt@kkg-do.eu>
2022-08-30 10:49:57 +0200

    ._.

--- src/Property.java
+++ src/Property.java
@@ -1,4 +1,15 @@
 public class Property {
 
     private String address;
+    private double width;
+    private double length;
+
+    public Property(){
+        address = "Erzbergerstraße 1";
+        width = 10;
+        length = 20.5;
+    }
 }
+
+
+
--- src/PropertyProject.java
+++ src/PropertyProject.java
@@ -1,7 +1,7 @@
 public class PropertyProject {
 
     public static void main(String[] args) {
-        System.out.println("Erzbergerstra. 1, 20m*10m=200m²)");
+        Property p1 = new Property();
     }
 
 }
