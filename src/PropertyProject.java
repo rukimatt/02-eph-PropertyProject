449c5e5b9da73b65031ec93be8512ba21af70f21
rukimatt <rukimatt@kkg-do.eu>
2022-08-30 11:21:57 +0200

    ._.

--- src/PropertyProject.java
+++ src/PropertyProject.java
@@ -4,16 +4,16 @@ public class PropertyProject {
         Property p1 = new Property("Erzbergerstraße 1", 10, 20.57);
         System.out.println(p1.getAddress() + " - " + p1.getSize() + "m²" );
 
-        Property p2 = new Property("Erzbergerstraße 2", 15, 22.212);
+        Property p2 = new Property("Mühlenstraße 2", 15, 22.212);
         System.out.println(p2.getAddress() + " - " + p2.getSize() + "m²" );
 
-        Property p3= new Property("Erzbergerstraße 3",17, 24.444);
+        Property p3= new Property("Hochaufstraße 3",17, 24.444);
         System.out.println(p3.getAddress() + " - " + p3.getSize() + "m²");
 
-        Property p4= new Property("Erzbergerstraße 4",5, 10.777);
+        Property p4= new Property("Nocheinestraße 4",5, 10.777);
         System.out.println(p4.getAddress() + " - " + p4.getSize() + "m²");
 
-        Property p5= new Property("Erzbergerstraße 5",18, 26.474);
+        Property p5= new Property("Yolostraße 5",18, 26.474);
         System.out.println(p5.getAddress() + " - " + p5.getSize() + "m²");
 
     }
