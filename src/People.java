e1175c046d542ffc41cf629b97cc83963c170164
rukimatt <rukimatt@kkg-do.eu>
2022-08-31 15:02:18 +0200

    ._.

---
+++ src/People.java
@@ -0,0 +1,19 @@
+public class People {
+
+    private String age;
+    private double height;
+    private double last_name;
+
+    public People(String a, double h, double l){
+        age = a;
+        height = h;
+        last_name = l;
+    }
+
+    public String getAge(){return age;}
+
+    public double getHeight(){return height;}
+
+    public double getLast_name(){return last_name;}
+
+}
